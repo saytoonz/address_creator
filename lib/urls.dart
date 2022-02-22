f6cea5bca5adf24c916229e556abb36415c3dd09
saytoonz <rassay31@gmail.com>
2022-02-22 09:52:07 +0000

    Uploaded to appstore

--- lib/urls.dart
+++ lib/urls.dart
@@ -3,7 +3,7 @@ import 'package:flutter/foundation.dart';
 class Urls {
   static String basedUrl = kReleaseMode
       ? "http://phplaravel-698707-2309199.cloudwaysapps.com/api"
-      : "http://127.0.0.01:8000/api";
+      : "http://phplaravel-698707-2309199.cloudwaysapps.com/api";
 
   static String getAddresses = "$basedUrl/get-addresses";
   static String saveAddress = "$basedUrl/save-address";
